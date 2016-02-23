9b2816b0a3cfff9cc13ad7a2cdf78a16a04a9b46
Antoine PAILLARD <paillard.antoine.ntes@gmail.com>
2016-02-23 14:37:11 +0100

    Init repo

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Antoine Paillard on 22/02/2016.
+//  Copyright © 2016 Antoine Paillard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func MakeMeNotSoUncool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        uncoolButton.hidden = true
+    }
+    
+
+}
+
